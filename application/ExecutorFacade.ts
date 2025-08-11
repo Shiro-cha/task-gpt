5c39cbca50cef01f97871eaf9a10e915da9988fa
Shiro-cha <noum.rzdr@gmail.com>
2025-08-11 20:23:26 +0300

    remove _ with name

--- application/ExecutorFacade.ts
+++ application/ExecutorFacade.ts
@@ -3,6 +3,6 @@ import type { Command } from "../models/Command";
 export class ExecutorFacade{
     constructor(private command: Command) {}   
     executeCommand(): string {
-        return `Command executed: ${this.command.name}`;
+        return `Command : ${this.command.task}`;
     }
 }
