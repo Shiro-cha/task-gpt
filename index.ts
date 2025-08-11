4beaa9c2378ff22e69502b366c7b4fa0af4d671c
Shiro <noum.rzdr@gmail.com>
2025-08-11 14:04:25 +0300

    exemple on index

--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
-console.log("Hello via Bun!");
+import { MessageFacade } from "./application/MessageFacade";
+import { Message } from "./models/Message";
+import { User } from "./models/User";
+
+const message = new Message("1", "Hello, how are you?", new Date(), new User("1", "John Doe", "YV7Gj@example.com", new Date()));
+const messageFacade = new MessageFacade(message);
+
+console.log(messageFacade.sendMessage());
+console.log(messageFacade.interpretMessage());
