a0acf52401a4f4730afed1fdd0bcf444c654c231
Shiro <noum.rzdr@gmail.com>
2025-08-12 16:42:35 +0300

    disable sent message

--- interfaces/cli/ConsoleIO.ts
+++ interfaces/cli/ConsoleIO.ts
@@ -2,19 +2,11 @@ import type { IUserIO } from "./IUserIO";
 
 export class ConsoleIO implements IUserIO {
   async question(prompt: string): Promise<string> {
-    await process.stdout.write(prompt); // no await needed for write
 
-    let input = "";
-    const buf = new Uint8Array(1);
-
-    const n = await process.stdin.read(buf);
-
-    const char = String.fromCharCode(buf[0]);
-
-    input += char;
-    
-
-    return input.trim();
+    process.stdout.write(prompt);
+    for await (const line of console) {
+      return line.toString().trim();
+    }
   }
 
   print(message: string): void {
