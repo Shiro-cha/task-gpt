3b3c181f4195942af908a14cd3c02004e3aab7d7
Shiro-cha <noum.rzdr@gmail.com>
2025-08-15 15:10:25 +0300

    fix __dirname error

--- interfaces/desktop/electron/main.ts
+++ interfaces/desktop/electron/main.ts
@@ -3,6 +3,9 @@ import path from "path";
 import isDev from "electron-is-dev";
 
 let mainWindow: BrowserWindow;
+const __filename = new URL(import.meta.url).pathname;
+const __dirname = path.dirname(__filename);
+
 
 function createWindow() {
   mainWindow = new BrowserWindow({
