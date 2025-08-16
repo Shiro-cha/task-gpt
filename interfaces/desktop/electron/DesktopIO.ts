8c96d48d9b952f7c96a523c36d9b8da7279a6b3b
Shiro-cha <noum.rzdr@gmail.com>
2025-08-16 14:13:45 +0300

    add question to Desktop IO

--- interfaces/desktop/electron/DesktopIO.ts
+++ interfaces/desktop/electron/DesktopIO.ts
@@ -4,12 +4,15 @@ import type { IUserIO } from "../../../domains/interfaces/IUserIO";
 
 export class DesktopIO implements IUserIO{
     constructor(private window: BrowserWindow) {}
+    question(prompt: string): Promise<string> {
+        throw new Error("Method not implemented.");
+    }
 
-  print(message: string): void {
+    print(message: string): void {
     this.window.webContents.send("bot:print", message);
-  }
+    }
 
-  close(): void {
+    close(): void {
     this.window.webContents.send("bot:close");
-  }
+    }
 }
