6bed251618b5a87c2f289b4d1bb33018dce59a95
Shiro-cha <noum.rzdr@gmail.com>
2025-08-17 15:18:18 +0300

    add app factory and bootstrap

---
+++ composition/bootstrapCore.ts
@@ -0,0 +1,26 @@
+import { ExecutorFacade } from "../application/ExecutorFacade";
+import { MessageFacade } from "../application/MessageFacade";
+import type { IHttpClient } from "../domains/interfaces/IHTTPClient";
+import type { IUserIO } from "../domains/interfaces/IUserIO";
+import type { Command } from "../domains/models/Command";
+import { User } from "../domains/models/User";
+import { appConfig } from "../infrastructures/config/appConfig";
+import { FetchHttpClient } from "../infrastructures/http/FetchHttpClient";
+import { GeminiProvider } from "../infrastructures/llm/GeminiProvider";
+import { ConsoleIO } from "../interfaces/cli/ConsoleIO";
+
+
+export type CoreDeps = {
+  messageFacade: MessageFacade;
+  executorFacadeFactory: (command: Command) => ExecutorFacade;
+  user: User;
+};
+export function bootstrapCore(): CoreDeps {
+    const httpClient: IHttpClient = new FetchHttpClient();
+    const llmProvider: GeminiProvider = new GeminiProvider(appConfig.gemini.apiUrl, appConfig.gemini.apiKey, httpClient);
+    const messageFacade: MessageFacade = new MessageFacade(undefined, llmProvider);
+    const executorFacadeFactory = (command: Command) => new ExecutorFacade(command);
+    
+    const user = new User("1", "Nomena", "nomena@gmail.com", new Date());
+    return { messageFacade, executorFacadeFactory, user };
+}
